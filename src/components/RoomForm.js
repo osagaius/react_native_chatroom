b3442510eb5678adc435cfce1566130f99e9d9a2
Osayame Gaius-Obaseki <osayame@gmail.com>
2017-07-11 12:37:56 -0400

    cleanup room form

--- src/components/RoomForm.js
+++ src/components/RoomForm.js
@@ -5,46 +5,16 @@ import { joinRoom } from '../actions';
 import { CardSection, Input } from './common';
 
 class RoomForm extends Component {
-  componentWillMount() {
-    this.props.joinRoom(this.props.room.name);
-  }
-
   render() {
     return (
       <View>
         <CardSection>
           <Input
             label="Name"
-            placeholder="Jane"
+            placeholder="German Sherpards Over Everything"
             value={this.props.name}
-            onChangeText={value => this.props.employeeUpdate({ prop: 'name', value })}
+            onChangeText={value => this.props.roomNameChanged({ name }}
           />
-        </CardSection>
-
-        <CardSection>
-          <Input
-            label="Phone"
-            placeholder="555-555-5555"
-            value={this.props.phone}
-            onChangeText={value => this.props.employeeUpdate({ prop: 'phone', value })}
-          />
-        </CardSection>
-
-        <CardSection style={{ flexDirection: 'column' }}>
-          <Text style={styles.pickerTextStyle}>Shift</Text>
-          <Picker
-            style={{ flex: 0 }}
-            selectedValue={this.props.shift}
-            onValueChange={value => this.props.employeeUpdate({ prop: 'shift', value })}
-          >
-            <Picker.Item label="Monday" value="Monday" />
-            <Picker.Item label="Tuesday" value="Tuesday" />
-            <Picker.Item label="Wednesday" value="Wednesday" />
-            <Picker.Item label="Thursday" value="Thursday" />
-            <Picker.Item label="Friday" value="Friday" />
-            <Picker.Item label="Saturday" value="Saturday" />
-            <Picker.Item label="Sunday" value="Sunday" />
-          </Picker>
         </CardSection>
       </View>
     );
@@ -52,10 +22,7 @@ class RoomForm extends Component {
 }
 
 const styles = {
-  pickerTextStyle: {
-    fontSize: 18,
-    paddingLeft: 20
-  }
+  
 };
 
 export default connect(null, { joinRoom })(RoomForm);
